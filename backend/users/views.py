219e1ea63461523a6d16c919365d7eb1677e9f61
Tedra <tbirch9@wgu.edu>
2025-07-07 12:45:46 -0400

    Resolved merge conflict in views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
 from django.http import JsonResponse
 
 def root_view(request):
     return JsonResponse({"message": "Welcome to the Clearlog API"})
->>>>>>> dev
